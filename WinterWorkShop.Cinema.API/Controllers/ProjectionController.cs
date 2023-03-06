377a661f2e59efc4504115eaaf696625b778095f
a.erdeljan-int <a.erdeljan-int@INTERNSHIP-ZR-8>
2023-03-06 14:57:17 +0100

    Added ProjectionModel, Projection Controller and ProjectionsResponse

---
+++ WinterWorkShop.Cinema.API/Controllers/ProjectionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WinterWorkShop.Cinema.API.Controllers
+{
+    public class ProjectionController : ControllerBase
+    {
+
+
+    }
+}
---
+++ WinterWorkShop.Cinema.Data/Models/ProjectionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterWorkShop.Cinema.Data.Models
+{
+    public class ProjectionModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int BrojSale { get; set; }
+        public DateTime DateTime { get; set; }
+    
+    }
+}
