0a1338c4a92f08f6a352cadca3c1d7d5f77c6cce
a.erdeljan-int <a.erdeljan-int@INTERNSHIP-ZR-8>
2023-03-06 14:38:09 +0100

    Added Description Field

--- WinterWorkShop.Cinema.Data/Database.cs
+++ WinterWorkShop.Cinema.Data/Database.cs
@@ -6,8 +6,8 @@ namespace WinterWorkShop.Cinema.Domain.Common
     {
         public List<MovieModel> GetAllMoviesResponses = new List<MovieModel> 
         { 
-            new MovieModel { Id = 1, Name = "Spuderman" },
-            new MovieModel { Id = 2, Name = "Spuderman 2"}
+            new MovieModel { Id = 1, Name = "Spuderman" , Description = "desc"},
+            new MovieModel { Id = 2, Name = "Spuderman 2", Description = "desc1"}
         };
     }
 }
