954e6d15615be953b697e3dc907085bf2698756e
a.erdeljan-int <a.erdeljan-int@INTERNSHIP-ZR-8>
2023-03-06 15:15:26 +0100

    Removed Unnecessary Using's

--- WinterWorkShop.Cinema.Data/Repositories/IProjectionRepository.cs
+++ WinterWorkShop.Cinema.Data/Repositories/IProjectionRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WinterWorkShop.Cinema.Data.Models;
+﻿using WinterWorkShop.Cinema.Data.Models;
 
 namespace WinterWorkShop.Cinema.Data.Repositories
 {
