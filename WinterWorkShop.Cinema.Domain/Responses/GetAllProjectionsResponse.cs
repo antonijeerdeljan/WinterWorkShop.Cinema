37078b49da8636cc0d50ba98c2cb0fdb2ce4b987
a.erdeljan-int <a.erdeljan-int@INTERNSHIP-ZR-8>
2023-03-06 15:12:41 +0100

    Fixed Description Bug

--- WinterWorkShop.Cinema.Domain/Responses/GetAllProjectionsResponse.cs
+++ WinterWorkShop.Cinema.Domain/Responses/GetAllProjectionsResponse.cs
@@ -10,9 +10,10 @@ namespace WinterWorkShop.Cinema.Domain.Responses
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        //public string Description { get; set; }
         public int BrojSale { get; set; }
         public DateTime DateTime { get; set; }
+
         public string Descripton { get; set; }
     }
 }
